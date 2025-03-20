cd055e354d4adbd20bf66931e3d0fba03129acfd
Catrix <catrix.kevyn@gmail.com>
2025-03-20 11:53:51 +0100

    laste

--- Module04/diaryapp/android/app/build.gradle.kts
+++ Module04/diaryapp/android/app/build.gradle.kts
@@ -1,47 +1,36 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    apply plugin: 'com.android.application'
 }
 
 android {
     namespace = "com.example.diaryapp"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 34
+    ndkVersion = "26.1.10909125"  // Dernière version stable du NDK
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
-        applicationId "com.auth0.samples"
-        minSdkVersion 21
-        targetSdkVersion flutter.targetSdkVersion
-        // ...
-
-        // ---> Add the next line
-        manifestPlaceholders += [auth0Domain: "dev-vekudtrpuzp0gs3i.eu.auth0.com", auth0Scheme: "https"]
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.diaryapp"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        manifestPlaceholders["auth0Domain"] = "dev-vekudtrpuzp0gs3i.eu.auth0.com"
+        manifestPlaceholders["auth0Scheme"] = "https"
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
