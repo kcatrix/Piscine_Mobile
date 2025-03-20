1673f2fe9e5816d2c009e529e22eef75d12b5501
Catrix <catrix.kevyn@gmail.com>
2025-03-20 12:03:21 +0100

    presque

--- Module04/diaryapp/lib/main.dart
+++ Module04/diaryapp/lib/main.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
 import 'services/auth_service.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Diary App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const AuthPage(),
+    );
+  }
+}
+
 class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
 
