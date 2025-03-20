cffc3b9bb8f24dd5e2de7a9cc190f01df4aad3ec
Catrix <catrix.kevyn@gmail.com>
2025-03-20 11:42:50 +0100

    auth

--- Module04/diaryapp/android/app/build.gradle.kts
+++ Module04/diaryapp/android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    apply plugin: 'com.android.application'
 }
 
 android {
@@ -20,6 +21,13 @@ android {
     }
 
     defaultConfig {
+        applicationId "com.auth0.samples"
+        minSdkVersion 21
+        targetSdkVersion flutter.targetSdkVersion
+        // ...
+
+        // ---> Add the next line
+        manifestPlaceholders += [auth0Domain: "dev-vekudtrpuzp0gs3i.eu.auth0.com", auth0Scheme: "https"]
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.diaryapp"
         // You can update the following values to match your application needs.
---
+++ Module04/diaryapp/lib/services/auth_service.dart
@@ -0,0 +1,33 @@
+import 'package:auth0_flutter/auth0_flutter.dart';
+
+class AuthService {
+  final Auth0 auth0 = Auth0(
+    'dev-vekudtrpuzp0gs3i.eu.auth0.com', // Ton domaine Auth0
+    '1DMEXYmuKMNUkAJM3LvaEygbpFVGNBBw'   // Ton client ID
+  );
+
+  Credentials? _credentials;
+
+  Future<UserProfile?> login() async {
+    try {
+      _credentials = await auth0.webAuthentication().login();
+      return _credentials?.user;
+    } catch (e) {
+      print('Erreur de connexion : $e');
+      return null;
+    }
+  }
+
+  Future<void> logout() async {
+    try {
+      await auth0.webAuthentication().logout();
+      _credentials = null;
+    } catch (e) {
+      print('Erreur de déconnexion : $e');
+    }
+  }
+
+  bool isAuthenticated() {
+    return _credentials != null;
+  }
+}
