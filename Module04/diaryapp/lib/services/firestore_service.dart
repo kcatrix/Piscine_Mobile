ba991cb443283a60efd2711b646253a3f39be315
Catrix <catrix.kevyn@gmail.com>
2025-03-21 23:58:15 +0100

    SaveNote

--- Module04/diaryapp/lib/services/firestore_service.dart
+++ Module04/diaryapp/lib/services/firestore_service.dart
@@ -11,6 +11,36 @@ class FirestoreService {
       'createdAt': FieldValue.serverTimestamp(), // Ajout de la date
     });
 
-    print("✅ Utilisateur enregistré dans Firestore : ${user.name}, ${user.email}, DateTime.now()}");
+    print("✅ Utilisateur enregistré dans Firestore : ${user.name}, ${user.email}, ${DateTime.now()}");
+  }
+
+  Future<void> saveNote(UserProfile user, notes) async{
+    await _db.collection('notes').doc(user.sub).set({
+      'name': user.name,
+      'email': user.email,
+      'createdAt': FieldValue.serverTimestamp(), // Ajout de la date
+      'Felling' : notes.Feeling,
+      'Title' : notes.title,
+      'Description' : notes.description
+    });
+  }
+
+  Future<List<Map<String, dynamic>>> getAllNotes() async {
+  try {
+    QuerySnapshot querySnapshot = await _db.collection('notes').get();
+    
+    List<Map<String, dynamic>> notes = querySnapshot.docs.map((doc) {
+      return {
+        'id': doc.id,
+        ...doc.data() as Map<String, dynamic>,
+      };
+    }).toList();
+
+    return notes;
+    }
+    catch (e) {
+    print("❌ Erreur lors de la récupération des notes : $e");
+    return [];
+    }
   }
 }
