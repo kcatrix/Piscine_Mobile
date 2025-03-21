7244f36eae26ac8c7f332f10f217414ca0749ee3
Catrix <catrix.kevyn@gmail.com>
2025-03-21 16:07:19 +0100

    diaryapp

---
+++ Module04/diaryapp/android/app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.diaryapp"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.diaryapp"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+
+        // Ajout des placeholders pour Auth0
+        manifestPlaceholders["auth0Domain"] = System.getenv("AUTH0_DOMAIN") ?: "dev-vekudtrpuzp0gs3i.eu.auth0.com"
+        manifestPlaceholders["auth0Scheme"] = System.getenv("AUTH0_CUSTOM_SCHEME") ?: "com.auth0.sample"
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
